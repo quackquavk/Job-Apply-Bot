5c4e02aadcb786cc61ffd69ce6a6753088e1f5a7
quackquavk <pukarkhanal4@gmail.com>
2025-04-23 17:07:21 +0545

    lalalal

--- complete8.py
+++ complete8.py
@@ -17,6 +17,8 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 from webdriver_manager.chrome import ChromeDriverManager
 from langchain_google_genai import GoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.safari.service import Service as SafariService
 
 # Load environment variables
 load_dotenv()
@@ -39,17 +41,13 @@ COOKIES_FILE = 'linkedin_cookies.pkl'
 RESUME_DATA_FILE = 'resume_data.json'
 
 def setup_driver():
-    """Set up and return a configured Chrome WebDriver"""
-    chrome_options = Options()
-    # Uncomment if you want to run in headless mode
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-gpu")
-    
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    """Set up and return a configured Safari WebDriver"""
+    safari_options = webdriver.SafariOptions()
+    # Then, in the Develop menu, ensure "Allow Remote Automation" is checked.
+
+    service = SafariService()  # No need for a separate driver manager
+    driver = webdriver.Safari(service=service, options=safari_options)
+    driver.maximize_window()
     return driver
 
 def save_cookies(driver):
